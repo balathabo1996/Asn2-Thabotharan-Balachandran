224b328453ceb265c4824f3a98a1a53a2b59c5d2
“Thabotharan <balathabo96@gmail.com>
2025-10-27 14:30:46 -0400

    Modify App.js

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.get('/allData', (req, res) => {
             airbnbData = [];
         }
         const selectedData = airbnbData.slice(0, 100);
-        res.render('pages/searchIndex', { title: 'Search Property by Index', data: selectedData });
+        res.render('pages/searchIndex', { title: '100 Airbnb Data', data: selectedData });
     }
     loadData();
 });
